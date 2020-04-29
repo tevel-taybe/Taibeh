949e2fa61aaef9706a7e7cb4ffe9137f6693c7b0
waheeb <wahib.ejmail@gmail.com>
2020-04-30 00:48:00 +0300

    Managing The Folders

--- 25.3.2020/25.3.2020/satellite-subsystems/demo/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lSatelliteSubsystems -lm -lHCC -lHAL -lFreeRTOS -lAt91
-
