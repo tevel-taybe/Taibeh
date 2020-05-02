da70f6c21549fee5b6f4e45652fb2e8c4cbc0f0b
waheeb ejmail <wahib.ejmail@gmail.com>
2020-05-02 22:21:24 +0300

    updating the General open issues

--- TaibehSatelliteProject/GlobusSatProject/src/Logger.h
+++ TaibehSatelliteProject/GlobusSatProject/src/Logger.h
@@ -9,20 +9,15 @@
 #include <SubSystemModules/Housekepping/TelemetryFiles.h>
 
 
-int logError(int error);
-
-int logInfo(char *info);
-
-#define E_CANT_TRANSMIT    		-200
-#define E_TOO_EARLY_4_BEACON    -201
-#define TRXVU_MUTE_TOO_LOMG    -202
-#define TRXVU_IDLE_TOO_LOMG    -203
-#define MAX_LOG_STR				40
-
-
-
-
-
+//int logError(int error);
+//
+//int logInfo(char *info);
+//
+//#define E_CANT_TRANSMIT    		-200
+//#define E_TOO_EARLY_4_BEACON    -201
+//#define TRXVU_MUTE_TOO_LOMG    -202
+//#define TRXVU_IDLE_TOO_LOMG    -203
+//#define MAX_LOG_STR				40
 
 #define 		MAX_LOG_CONTENT			64
 #define 		MAX_LOG_COMPONENT_NAME 	16
